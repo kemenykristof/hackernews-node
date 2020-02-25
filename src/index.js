a72ff9dd5c467ca066873cb6c75bdff85205c22a
kemenykristof <kemeny.kristof24@gmail.com>
2020-02-25 16:11:24 +0100

    resolvers clean up, schema exported to its own file

--- src/index.js
+++ src/index.js
@@ -7,39 +7,29 @@ let links = [
     description: "Fullstack tutorial for GraphQL"
   }
 ];
-
 // 1
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
-
-// 2
+let idCount = links.length;
 const resolvers = {
-    Query: {
-      info: () => `This is the API of a Hackernews Clone`,
-      // 2
-      feed: () => links,
-    },
-    // 3
-    Link: {
-      id: (parent) => parent.id,
-      description: (parent) => parent.description,
-      url: (parent) => parent.url,
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
     }
   }
+};
 
-// 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
