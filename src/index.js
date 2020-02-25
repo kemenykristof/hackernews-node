14801874ff6329f79e143afede897cc6908aabcf
kemenykristof <kemeny.kristof24@gmail.com>
2020-02-25 14:19:13 +0100

    some dummy data and resolvers update to retrieve links

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const { GraphQLServer } = require("graphql-yoga");
 
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+
 // 1
 const typeDefs = `
 type Query {
@@ -12,14 +20,22 @@ type Link {
   description: String!
   url: String!
 }
-`
+`;
 
 // 2
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    Query: {
+      info: () => `This is the API of a Hackernews Clone`,
+      // 2
+      feed: () => links,
+    },
+    // 3
+    Link: {
+      id: (parent) => parent.id,
+      description: (parent) => parent.description,
+      url: (parent) => parent.url,
+    }
   }
-};
 
 // 3
 const server = new GraphQLServer({
