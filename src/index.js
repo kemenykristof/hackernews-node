3ebc217a3a4a9d0f969abbacf5fa41edcdd48cf0
kemenykristof <kemeny.kristof24@gmail.com>
2020-02-27 13:45:35 +0100

    resolver for link subscription added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
+const Subscription = require('./resolvers/Subscription')
+
 
 const resolvers = {
   Query,
